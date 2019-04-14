d62b367117ec94cd4ac3a3ea621d95147b67a4cf
rlu789 <rlu789@hotmail.com>
2019-04-14 22:07:05 +1000

    my bad jon

---
+++ Assets/Scripts/Input/KeyPessApplyMotion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class KeyPessApplyMotion : MonoBehaviour
+{
+    [SerializeField] float speedModifier = 0.1f;
+
+    private Rigidbody2D rgb;
+    private Vector2 velocity;
+
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal") * speedModifier;
+        float vertical = Input.GetAxisRaw("Vertical") * speedModifier;
+        rgb.MovePosition(new Vector2(rgb.position.x + horizontal, rgb.position.y + vertical));
+    }
+}
