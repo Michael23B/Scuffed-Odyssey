8ac060163de699881a0f35e21e4eb2625f5e3ff6
Jon Alfaro <ljjalfaro@gmail.com>
2019-04-14 13:46:13 +1000

    Procedural Playground Added

---
+++ Assets/Scripts/Procedural/TileAutomate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileAutomate : MonoBehaviour
+{
+    public Tile bTile;
+
+    public Tilemap tmWorld;
+
+    private int[,] worldArray = new int[10, 10];
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        worldArray = new int[10, 10];
+        worldArray.SetRow(9, 1);
+        worldArray.PrettyPrint2DArray();
+        for (int x = 0; x < worldArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < worldArray.GetLength(1); y++)
+            {
+                if (worldArray[x, y] == 1)
+                {
+                    tmWorld.SetTile(new Vector3Int(y, -x, 0), bTile);
+                }
+            }
+        }
+//        tmWorld.SetTile(new Vector3Int(0, 0,0), bTile);
+    }
+}
---
+++ Assets/Scripts/Utils/UtilityMethods.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA;
+
+public static class UtilityMethods
+{
+    // Initializes every element of an array to the provided value
+    public static T[] InitializeArray<T>(this T[] arr, T value)
+    {
+        for (int i = 0; i < arr.Length; ++i)
+        {
+            arr[i] = value;
+        }
+
+        return arr;
+    }
+
+    // Initializes every element of a 2D array to the provided value
+    // int[,]  int, WorldArray type[,] WorldArray, int: 0
+    public static T[,] Initialize2DArray<T>(this T[,] arr, T value)
+    {
+        for (int i = 0; i < arr.GetLength(0); ++i)
+        {
+            for (int j = 0; j < arr.GetLength(1); ++j)
+            {
+                arr[i, j] = value;
+            }
+        }
+
+        return arr;
+    }
+
+    public static T[,] SetRow<T>(this T[,] arr, int row, T value)
+    {
+        for(int i = 0; i < arr.GetLength(0); ++i)
+        {
+            if (i == row)
+            {
+                for (int j = 0; j < arr.GetLength(1); ++j)
+                {
+                    arr[i, j] = value;
+                }
+
+                break;
+            }
+        }
+
+        return arr;
+    }
+
+    public static void PrettyPrint2DArray<T>(this T[,] matrix)
+    {
+        string msgLine = "Printing 2D Array\nCLICK TO EXPAND\n{ ";
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                msgLine = $"{msgLine}{matrix[i, j]}, ";
+//                Debug.Log(matrix[i, j] + "\t");
+            }
+
+            msgLine = msgLine.TrimEnd(',', ' ');
+            msgLine = $"{msgLine} }}\n{{ ";
+        }
+
+            Debug.Log(msgLine.TrimEnd('{', ' ')+ "\n");
+    }
+}
+
+
