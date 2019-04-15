8aa09a283b42d6206288a923d9b2d4e81f38a600
Jon Alfaro <ljjalfaro@gmail.com>
2019-04-15 22:05:56 +1000

    Scuffed Procedural Map

---
+++ Assets/Scripts/Procedural/TileTypes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TileTypes
+{
+    public enum BiomeTypes { Desert = 0, Plains = 1, Rock = 2};
+    public struct TileInfo
+    {
+        public bool Created;
+        public BiomeTypes Biome;
+        public byte[] BiomeWeights;
+
+        public TileInfo(byte[,] previousWeights)
+        {
+            Created = true;
+            BiomeWeights  = new byte[3];
+
+            for (int i = 0; i < previousWeights.GetLength(0); i++)
+            {
+                for (int j = 0; j < previousWeights.GetLength(1); j++)
+                {
+                    if (BiomeWeights[j] <= previousWeights[i, j])
+                    {
+                        BiomeWeights[j] = previousWeights[i, j];
+                    }
+                }
+                
+            }
+
+            int biomeIndex = 0;
+            for (int o = 0; o < BiomeWeights.GetLength(0); o++)
+            {
+                if (BiomeWeights[biomeIndex] <= BiomeWeights[o])
+                {
+                    biomeIndex = o;
+                }
+            }
+
+            Biome = (BiomeTypes)biomeIndex;
+        }
+    }
+}
