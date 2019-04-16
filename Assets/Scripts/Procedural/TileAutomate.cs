f4b3ddd5f6fb4ca72bf2adfe5a322f59d60e384a
Jon Alfaro <ljjalfaro@gmail.com>
2019-04-16 20:49:59 +1000

    WorldBlock Class Added

--- Assets/Scripts/Procedural/TileAutomate.cs
+++ Assets/Scripts/Procedural/TileAutomate.cs
@@ -10,92 +10,22 @@ public class TileAutomate : MonoBehaviour
     public Tile[] bTile;
 
     public Tilemap tmWorld;
-    private static readonly int xySize = 50;
-    private int[,] worldArray = new int[xySize, xySize];
-
-    public TileTypes.TileInfo[,] tileOfHell = new TileTypes.TileInfo[xySize, xySize];
-    private System.Random r = new System.Random();
+    public WorldBlock worldBlock;
 
     // Start is called before the first frame update
     private void Start()
     {
-        int[,] sOfHell = new int[4,2]{{-1,0},{1,0},{0,-1},{0,1}};
-
-        tileOfHell[0,0].BiomeWeights = new byte[]{0,20,0};
-        tileOfHell[0, 0].Biome = TileTypes.BiomeTypes.Plains;
-        for (int x = 0; x < tileOfHell.GetLength(0); x++)
+        worldBlock = new WorldBlock(25,25,65456465);
+        for (int x = 0; x < worldBlock.MaxX; x++)
         {
-            for (int y = 0; y < tileOfHell.GetLength(1); y++)
+            for (int y = 0; y < worldBlock.MaxY; y++)
             {
-                // Check Surroundings
-                int[] currentWeights = new int[3];
-                for (int i = 0; i < sOfHell.GetLength(0); i++)
-                {
-                    int modX = sOfHell[i,0] + x;
-                    int modY = sOfHell[i, 1] + y;
-
-                    if (modX < 0 || modY < 0 || modX >= xySize || modY >= xySize)
-                    {
-                        continue;
-                    }
-                    else if (!tileOfHell[modX,modY].Created)
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        for (int wI = 0; wI < tileOfHell[modX, modY].BiomeWeights.GetLength(0); wI++)
-                        {
-                            currentWeights[wI] += tileOfHell[modX, modY].BiomeWeights[wI];
-                        }
-                    }
+                if (!(x == 0 && y == 0)) {
+                worldBlock.SetTile(x,y);
                 }
-
-                int maxWeight = 0;
-                int maxIndex = 0;
-                for (int i = 0; i < currentWeights.GetLength(0); i++)
-                {
-                    if (currentWeights[i] == 0)
-                    {
-                        currentWeights[i] = (byte)r.Next(0, 30);
-                    }
-
-                    if (maxWeight <= currentWeights[i])
-                    {
-                        maxWeight = currentWeights[i];
-                        maxIndex = i;
-                    }
-                }
-                if  (currentWeights[maxIndex] > 254)
-                {
-                    currentWeights[maxIndex] = 254;
-                }
-
-                currentWeights[maxIndex] = (int)(currentWeights[maxIndex]*0.9);
-
-                // Tile
-                tileOfHell[x, y].Created = true;
-                tileOfHell[x, y].BiomeWeights = new byte[]{(byte)currentWeights[0], (byte)currentWeights[1], (byte)currentWeights[2] };
-                tileOfHell[x, y].Biome = (TileTypes.BiomeTypes) maxIndex;
-
-                tmWorld.SetTile(new Vector3Int(y, -x, 0), bTile[maxIndex]);
-                
+                tmWorld.SetTile(new Vector3Int(y, -x, 0), bTile[(int)worldBlock.Tile[x,y].Biome]);
             }
         }
 
-            //        worldArray = new int[10, 10];
-            //        worldArray.SetRow(9, 1);
-            //        worldArray.PrettyPrint2DArray();
-            //        for (int x = 0; x < worldArray.GetLength(0); x++)
-            //        {
-            //            for (int y = 0; y < worldArray.GetLength(1); y++)
-            //            {
-            //                if (worldArray[x, y] == 1)
-            //                {
-            //                    tmWorld.SetTile(new Vector3Int(y, -x, 0), bTile);
-            //                }
-            //            }
-            //        }
-            //        tmWorld.SetTile(new Vector3Int(0, 0,0), bTile);
     }
 }
---
+++ Assets/Scripts/Procedural/WorldBlock.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class WorldBlock
+{
+    public TileTypes.TileInfo[,] Tile;
+    private System.Random _r;
+    private int _maxX, _maxY;
+    private int[,] _sMod = new int[4, 2] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
+    public WorldBlock(int x, int y, int seed)
+    {
+        _maxX = x;
+        _maxY = y;
+        _r = new System.Random(seed);
+        Tile = new TileTypes.TileInfo[x,y];
+
+        // Init First Tile
+        Tile[0, 0].BiomeWeights = new byte[] { 0, 20, 0 };
+        Tile[0, 0].Biome = TileTypes.BiomeTypes.Plains;
+        Tile[0, 0].Created = true;
+    }
+
+    public void SetTile(int x, int y)
+    {
+        int[,] surroundingXy = modXY(x, y);
+        int[] currentWeights = new int[3];
+
+        // Get Surrounding Weights
+        for (int i = 0; i < surroundingXy.GetLength(0); i++)
+        {
+            int modX = surroundingXy[i, 0];
+            int modY = surroundingXy[i, 1];
+            
+            // Get if index is out of bounds
+            if (modX < 0 || modY < 0 || modX >= _maxX || modY >= _maxY)
+            {
+                continue;
+            }
+            else if (!Tile[modX, modY].Created)
+            {
+                continue;
+            }
+            else
+            {
+                for (int wI = 0; wI < Tile[modX, modY].BiomeWeights.GetLength(0); wI++)
+                {
+                    currentWeights[wI] += Tile[modX, modY].BiomeWeights[wI];
+                }
+            }
+        }
+
+        // Calculate New Weights
+        int maxWeight = 0;
+        int maxIndex = 0;
+
+        for (int i = 0; i < currentWeights.GetLength(0); i++)
+        {
+            if (currentWeights[i] == 0)
+            {
+                currentWeights[i] = (byte)_r.Next(0, 20);
+            }
+
+            if (maxWeight <= currentWeights[i])
+            {
+                maxWeight = currentWeights[i];
+                maxIndex = i;
+            }
+        }
+        if (currentWeights[maxIndex] > 254)
+        {
+            currentWeights[maxIndex] = 254;
+        }
+
+        currentWeights[maxIndex] = (int)(currentWeights[maxIndex] * 0.9);
+
+//        for (int i = 0; i < currentWeights.GetLength(0); i++)
+//        {
+//            if (i == maxIndex)
+//            {
+//                continue;
+////                currentWeights[i] = Mathf.Clamp(currentWeights[i] - _r.Next(0, 2), 0, 254);
+//            }
+//
+//            currentWeights[i] = Mathf.Clamp(currentWeights[i] + _r.Next(0, 6), 0, 254);
+//        }
+
+
+        // Set Tile
+        Tile[x, y].Created = true;
+        Tile[x, y].BiomeWeights = new byte[] { (byte)currentWeights[0], (byte)currentWeights[1], (byte)currentWeights[2] };
+        Tile[x, y].Biome = (TileTypes.BiomeTypes)maxIndex;
+    }
+
+    // TODO:
+    public void DeNoise()
+    {
+        for (int x = 0; x < _maxX; x++)
+        {
+            for (int y = 0; y < _maxY; y++)
+            {
+                
+            }
+        }
+    }
+
+    // TODO: Calculate region/chunk stats
+    public void CalcRegionStats()
+    {
+
+    }
+
+    public int MaxX => _maxX;
+    public int MaxY => _maxY;
+
+    private int[,] modXY(int x, int y)
+    {
+        int[,] moddedXyList = _sMod;
+        for (int i = 0; i < _sMod.GetLength(0); i++)
+        {
+            moddedXyList[i,0] = x + _sMod[i,0];
+            moddedXyList[i, 1] = y + _sMod[i, 1];
+        }
+
+        return moddedXyList;
+    }
+
+}
